e942ce005737d21863f0c19b0d32ce148c51fe64
le anh quan <laquan1311@gmail.com>
2022-09-08 10:51:32 +0700

    thuc-hanh-su-dung-toan-tu

---
+++ thuc-hanh-su-dung-toan-tu.js
@@ -0,0 +1,8 @@
+let inputWitdh;
+let inputHeight;
+inputWitdh = prompt('Enter the Witdh');
+inputHeight = prompt('Enter the Height');
+let Witdh = parseInt(inputWitdh);
+let Height = parseInt(inputHeight);
+let area = Witdh * Height
+document.write('The area is' + area)
